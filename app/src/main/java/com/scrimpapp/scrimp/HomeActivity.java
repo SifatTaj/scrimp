7997cec6c909fbeae7e433baa8135d00a3e0a69c
TAJ <sifat.taj@gmail.com>
2019-03-18 22:44:14 +0600

    match fix

--- app/src/main/java/com/scrimpapp/scrimp/HomeActivity.java
+++ app/src/main/java/com/scrimpapp/scrimp/HomeActivity.java
@@ -2,6 +2,9 @@ package com.scrimpapp.scrimp;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -12,18 +15,82 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Nullable;
 
 public class HomeActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private Marker marker;
-    private TextView tvLatLng;
+    private TextView tvLatLng, tvMatches;
+    private FirebaseFirestore mFirestore;
+    private List<String> matchesList;
+    private Button btSetDest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         tvLatLng = findViewById(R.id.tvLatLng);
+        tvMatches = findViewById(R.id.tvNoOfMatches);
+        btSetDest = findViewById(R.id.btSetDest);
+
+        matchesList = new ArrayList<>();
+
+        mFirestore = FirebaseFirestore.getInstance();
+
+        mFirestore.collection("users").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(@Nullable QuerySnapshot queryDocumentSnapshots, @Nullable FirebaseFirestoreException e) {
+                for(DocumentChange doc : queryDocumentSnapshots.getDocumentChanges()) {
+
+                  if(doc.getType() == DocumentChange.Type.ADDED | doc.getType() == DocumentChange.Type.MODIFIED) {
+
+                      try {
+                          if(!doc.getDocument().getString("user_id").equals(FirebaseAuth.getInstance().getCurrentUser().getUid())) {
+                              if(doc.getDocument().getString("destination").equals("uttara")) {
+
+                                  if(doc.getDocument().getBoolean("online")) {
+                                      matchesList.add(doc.getDocument().getString("user_id"));
+                                      tvMatches.setText(matchesList.size() + " found");
+                                  }
+                                  else if (!doc.getDocument().getBoolean("online")){
+                                      if(matchesList.contains(doc.getDocument().getString("user_id"))) {
+                                          matchesList.remove(doc.getDocument().getString("user_id"));
+                                          tvMatches.setText(matchesList.size() + " found");
+                                      }
+                                  }
+
+                              }
+                          }
+                      } catch (NullPointerException ne) {
+                          ne.printStackTrace();
+                      }
+
+                  }
+
+                }
+            }
+        });
+
+        btSetDest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
