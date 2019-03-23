e72bc2e500257502d374363b34461a7b5744398a
TAJ <sifat.taj@gmail.com>
2019-03-23 16:36:51 +0600

    User profile api connected

--- app/src/main/java/com/scrimpapp/scrimp/UserProfileActivity.java
+++ app/src/main/java/com/scrimpapp/scrimp/UserProfileActivity.java
@@ -6,20 +6,34 @@ import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import javax.annotation.Nullable;
+
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class UserProfileActivity extends AppCompatActivity {
 
     CircleImageView dpImage;
+    String dpUrl;
 
-    String dpUrl = "https://firebasestorage.googleapis.com/v0/b/scrimp-33075.appspot.com/o/profilephoto1.jpg?alt=media&token=5d9d959f-79df-4116-92c2-d7c9307dd90d";
+    FirebaseAuth mAuth;
+    FirebaseFirestore mFirebaseFirestore;
+
+    TextView tvName, tvPhone, tvEmail, tvBracuId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +42,36 @@ public class UserProfileActivity extends AppCompatActivity {
 
         dpImage = findViewById(R.id.profileImage);
 
-        new DpLoader(getApplicationContext(), dpImage).execute(dpUrl);
-        }
+        tvName = findViewById(R.id.tvUserProfileName);
+        tvPhone = findViewById(R.id.tvUserProfilePhone);
+        tvEmail = findViewById(R.id.tvUserProfileEmail);
+        tvBracuId = findViewById(R.id.tvUserProfileBracuId);
 
+        mAuth = FirebaseAuth.getInstance();
+        mFirebaseFirestore = FirebaseFirestore.getInstance();
+
+        String userId = mAuth.getCurrentUser().getUid();
+
+        DocumentReference userInfo = mFirebaseFirestore.collection("users").document(userId);
+
+        userInfo.addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+
+                if (documentSnapshot != null) {
+                    tvName.setText(documentSnapshot.getString("name"));
+                    tvPhone.setText(documentSnapshot.getString("phone"));
+                    tvEmail.setText(documentSnapshot.getString("email"));
+                    tvBracuId.setText(documentSnapshot.getString("bracu_id"));
+
+                    dpUrl = documentSnapshot.getString("dp_url");
+                    new DpLoader(getApplicationContext(), dpImage).execute(dpUrl);
+
+                }
+            }
+        });
+
+    }
 }
 
 class DpLoader extends AsyncTask<String, Void, Bitmap> {
@@ -54,7 +95,7 @@ class DpLoader extends AsyncTask<String, Void, Bitmap> {
             Bitmap myBitmap = BitmapFactory.decodeStream(input);
             return myBitmap;
         } catch (IOException e) {
-            Toast.makeText(context, "Could not load profile image", Toast.LENGTH_LONG).show();
+//            Toast.makeText(context, "Could not load profile image", Toast.LENGTH_LONG).show();
             return null;
         }
     }
